bfcb871f2e40dfd2d6e2d22c40248fa03be3ad15
Oliver <oliver@oliver-ThinkPad-X230-Tablet.(none)>
2014-03-18 20:27:45 -0500

    I began to add changes to test if there were certain cites that were better for vocab words.

--- findVocabWords.py
+++ findVocabWords.py
@@ -27,6 +27,12 @@ import GoogleScraper
 import urllib.parse
 
 
+vocab_hint_list = ["http://dictionary.cambridge.org/us/dictionary/american-english/","http://dictionary.reference.com/","http://en.wiktionary.org/wiki/","http://mnemonicdictionary.com/word/","http://thesaurus.com/browse/","http://www.collinsdictionary.com/dictionary/english/","http://www.dict.cc/?s=","http://www.macmillandictionary.com/us/dictionary/american/","http://www.merriam-webster.com/dictionary/","http://www.oxforddictionaries.com/us/definition/american_english/","http://www.thefreedictionary.com/","http://www.vocabulary.scom/dictionary/","http://www.wordreference.com/definition/"]
+
+
+
+vocab_list = set()
+
 if __name__=='__main__':
 	dictionary = dict()
 	trash = set()
@@ -74,22 +80,36 @@ if __name__=='__main__':
 
 	#actually sort the items from the dictionary
 	smart_words = sorted(dictionary.items(), key=operator.itemgetter(1,0),reverse=True)
-	
+	num_words = 100	
 	#print out the first 100 words to see if anythings worth it
-	for word in smart_words[:199]:
-		print( word ) 	
-	
-	results = GoogleScraper.scrape("exacerbate",num_results_per_page=20, num_pages=1, offset=0, searchtype='normal')
-
-	for page in results:
+	for word in smart_words[:num_words]:
+		
+		results = GoogleScraper.scrape(word[0],num_results_per_page=20, num_pages=1, offset=0, searchtype='normal')
+		print("W: %s" % word[0])
+		urls = []
+		hint_count = 0
+		#need at least two dictionary sites to be classified as a dictionary word
+		hint_threshold = 2
+		page = results[0]
+		
 		for link_title, link_snippet, link_url in page['results']:
 		    # You can access all parts of the search results like that
-		    # link_url.scheme => URL scheme specifier (Ex: 'http')
-		    # link_url.netloc => Network location part (Ex: 'www.python.org')
-		    # link_url.path => URL scheme specifier (Ex: ''help/Python.html'')
-		    # link_url.params => Parameters for last path element
-		    # link_url.query => Query component
 			try:
-				print(urllib.parse.unquote(link_url.geturl())) # This reassembles the parts of the url to the whole thing
+				urls.append(urllib.parse.unquote(link_url.geturl()))
 			except:
 				pass
+
+		for hint in vocab_hint_list:
+			for site in urls[:3]:
+				if hint in site:
+					hint_count += 1
+					#vocab_list.add(word[0])
+					print("\tHint:%s" % hint[7:])
+		
+		if(hint_count >= hint_threshold):
+			vocab_list.add(word[0])
+			print("\t\tAdded %s to vocab list" % word[0])
+		
+	
+	print(vocab_list)	
+	print("Found %d words as vocab words out of %d" % (len(vocab_list),num_words))
